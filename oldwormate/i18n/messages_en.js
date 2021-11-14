2df9d6e4c481cd287445f1c37876c8c40d0da8ae
Daboss7173 <75582409+Daboss7173@users.noreply.github.com>
2021-11-13 19:40:51 -0600

    Uploaded Old Wormate build
    There are no private servers quite yet so the physics and hitboxes are still the newer versions

---
+++ oldwormate/i18n/messages_en.js
@@ -0,0 +1,80 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function (w) {
+    w.I18N_LANG = 'en';
+    var _ = w.I18N_MESSAGES = {};
+
+    _["index.game.leader.top10"] = "Top 10";
+
+    _["index.game.result.title"]    = "Congratulations!";
+    _["index.game.result.continue"] = "Continue";
+    _["index.game.result.share"]    = "Share";
+
+    _["index.game.result.your"]         = "Your final worm's weight was";
+    _["index.game.result.place.i1"]     = "1st";
+    _["index.game.result.place.i2"]     = "2nd";
+    _["index.game.result.place.i3"]     = "3rd";
+    _["index.game.result.place.i4"]     = "4th";
+    _["index.game.result.place.i5"]     = "5th";
+    _["index.game.result.place.i6"]     = "6th";
+    _["index.game.result.place.i7"]     = "7th";
+    _["index.game.result.place.i8"]     = "8th";
+    _["index.game.result.place.i9"]     = "9th";
+    _["index.game.result.place.i10"]    = "10th";
+    _["index.game.result.placeInBoard"] = "Place in the leader board!";
+    _["index.game.result.tryHit"]       = "Let's play again?";
+
+    _["index.game.social.shareResult.caption"]  = "I got a new high-score on wormate.io";
+    _["index.game.social.shareResult.messGood"] = "My worm '{0}' gained {1} points of weight and hit {2} place in the leader board! Could you beat it? #wormate #wormateio";
+    _["index.game.social.shareResult.messNorm"] = "My worm '{0}' gained {1} points of weight! Could you beat it? #wormate #wormateio";
+
+    _["index.game.main.menu.unlockSkins.share"]       = "Share";
+    _["index.game.main.menu.unlockSkins.comeAndPlay"] = "Come and play";
+
+    _["index.game.popup.menu.store.tab"] = "Store";
+    _["index.game.popup.menu.coins.tab"] = "Coins";
+    _["index.game.popup.menu.skins.tab"] = "Skins";
+
+    _["util.time.days"]  = "days";
+    _["util.time.hours"] = "hours";
+    _["util.time.min"]   = "min";
+    _["util.time.sec"]   = "sec";
+
+    /*Compatibility:*/
+    _["index.game.result.place"]        = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"];
+})(window);
+
+}
+/*
+     FILE ARCHIVED ON 09:03:29 Mar 11, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:28:42 Nov 08, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1556.536
+  exclusion.robots: 364.093
+  exclusion.robots.policy: 364.081
+  xauthn.identify: 312.451
+  xauthn.chkprivs: 51.452
+  RedisCDXSource: 6.85
+  esindex: 0.008
+  LoadShardBlock: 1170.433 (3)
+  PetaboxLoader3.datanode: 996.614 (4)
+  CDXLines.iter: 12.643 (3)
+  load_resource: 76.204
+  PetaboxLoader3.resolve: 30.267
+*/
