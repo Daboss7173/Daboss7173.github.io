3eefd7792ead79cc997c687548cf3444073ffdad
Daboss7173 <75582409+Daboss7173@users.noreply.github.com>
2023-09-23 11:50:33 -0500

    Added some mod adjustments
    Added in star bar adjustment for the fast game speed mod.
    
    Added a rebalanced battles mod, which makes higher-leveled players more powerful.

--- oldprodigy/1-50-0/js/mod-handler.js
+++ oldprodigy/1-50-0/js/mod-handler.js
@@ -7,6 +7,10 @@ var GameMods = {
 		{
 			id: "FastGameSpeed",
 			patch: "initFastGameSpeedMod"
+		},
+		{
+			id: "RebalancedBattles",
+			patch: "initRebalancedBattleMod"
 		}
 	]
 };
@@ -42,7 +46,7 @@ class ModHandler {
 		this.game.prodigy.player.walkSpeed = 1,
 		Prodigy.Container.CreatureContainer.prototype.setPath = function (e, t, i) {
 			if (Util.isDefined(e)) {
-				this.game.tweens.removeFrom(this, !1), this.game.tweens.removeFrom(this.sprites), Util.isDefined(i) || (i = this.game.prodigy.player.walkSpeed);
+				this.game.tweens.removeFrom(this, !1), this.game.tweens.removeFrom(this.sprites), Util.isDefined(i) || (i = this.game.prodigy.player.walkSpeed * this.walkSpeed);
 				for (var a = null, s = null, r = this.x, o = this.y, n = e.length - 1; n >= 0; n--) {
 					var h = e[n];
 					Util.isDefined(h.x) || (h.x = r), Util.isDefined(h.y) || (h.y = o);
@@ -80,7 +84,8 @@ class ModHandler {
 	initFastGameSpeedMod() {
 		var i = Phaser.TweenManager.prototype.add,
 			e = this.game,
-			t = 3; // Speed multiplier
+			t = 3, // Speed multiplier
+			S = 3; // Star bar speed multiplier
 		Phaser.TweenManager.prototype.add = function(a) {
 			a.timeScale = t, i.call(this, a)
 		};
@@ -91,6 +96,94 @@ class ModHandler {
 		var s = Phaser.Tween.prototype.delay;
 		Phaser.Tween.prototype.delay = function(a, b) {
 			a /= t; return s.call(this, a, b)
+		},
+		Prodigy.Container.Hearts.prototype.updateStarsBar = function (e) {
+			var t = Prodigy.Creature.starsToLevel(this.level > 0 ? this.level - 1 : 0);
+			this.currentStars < t && (this.currentStars = t);
+			var i = Math.round((this.currentStars - t) / (this.starsToLevel + (this.currentStars - t)) * 100) / 100;
+			if (this.level >= 100 && (i = 1), i !== this.starPercentage) {
+				var a = 20,
+					s = this.reverse ? this.starsBase.x + Math.round((1 - i) * (278 - a)) : this.starsBase.x - Math.round((1 - i) * (278 - a));
+				if (e) {
+					if (i < this.starPercentage) {
+						var r = this.game.add.tween(this.starsFill).to({
+								x: this.starsBase.x
+							}, 1e3, Phaser.Easing.Quadratic.Out, !1, 700 / S),
+							o = this.game.add.tween(this.starsFill).to({
+								alpha: 0
+							}, 100, Phaser.Easing.Linear.None),
+							n = this.game.add.tween(this.starsFill).to({
+								x: s
+							}, 1e3, Phaser.Easing.Quadratic.Out);
+						o.onComplete.add(function () {
+							this.starsFill.alpha = 1, this.starsFill.x = this.starsBase.x + (this.reverse ? 278 : -278)
+						}, this), r.chain(o), o.chain(n), r.start() 
+					} else {
+						this.game.add.tween(this.starsFill).to({
+							x: s
+						}, 1e3, Phaser.Easing.Quadratic.Out, !0, 700 / S)
+					};
+					this.bumpStars()
+				} else this.starsFill.x = s;
+				this.starPercentage = i
+			}
+		},
+		Prodigy.Container.Hearts.prototype.bumpStars = function () {
+			this.game.add.tween(this.starsIcon.scale).to({
+				y: 1.4,
+				x: 1.4
+			}, 220, Phaser.Easing.Quadratic.Out, !0, 800 / S, 2, !0)
+		}
+	}
+	
+	initRebalancedBattleMod() {
+		Prodigy.Attacks.prototype.calculateDamage = function (e, t, i, a, s) {
+			Util.isDefined(a) || (a = 0);
+			var r = 0;
+			Util.isDefined(e) && Util.isDefined(e.damage) && (r = e.damage);
+			var o = e.element,
+				n = i.getElement(),
+				l = 0,
+				f = 0;
+			Util.isDefined(t) ? (l = 1 + (t.getLevel() - 1), f = (l + 13) / 14) : (f = 1);
+			return "Glacial Shield" !== e.name && 0 === i.modifiers.ignoreElement && (this.isStrong(o, n) ? r += 4 : this.isWeak(o, n) && (r -= 3, 0 >= r && (r = 2))), r = Math.floor(10 * r * s * f), Math.max(Math.floor(r + (Util.isDefined(a) ? a : 0)), 0)
+		},
+		Prodigy.Creature.getHeartsFromCurve = function (e, t, i) {
+			var a = Prodigy.Creature.HP_BONUS[e],
+				s = Math.floor((20 + a) * i / 20),
+				r = Math.floor((20 + a) * (t - 1) / 20);
+			return 20 * (s - r)
+		},
+		Attack.prototype.damage = function () {
+			this.calculateDamageDone(), Util.isDefined(this.atk.type) && "epic-attack" === this.atk.type && (Util.isDefined(this.epic) ? this.game.prodigy.effects.characterImage(this.game.prodigy.create.sprite(this.epic.x, this.epic.y - 50, "battle", "text-epic-attack")) : this.game.prodigy.effects.characterImage(this.game.prodigy.create.sprite(this.source.x, this.source.y - 50, "battle", "text-epic-attack"))), this.potionActive = !1, this.shieldTime = 0;
+			var e = 0;
+			if (Util.isDefined(this.target.source.modifiers) && Util.isDefined(this.target.source.modifiers.potion)) {
+				var t = Items.getItemData("item", this.target.source.modifiers.potion);
+				if (Util.isDefined(t.subType) && "elemental" === t.subType && Util.isDefined(this.atk.element) && ("all" === t.element || this.atk.element === t.element)) {
+					this.potionActive = !0, this.shieldTime = 1e3, e = t.potency * this.damageDone / 10, this.damageDone -= Math.round(e);
+					var i = this.target.sprites.add(this.game.prodigy.create.sprite(0, -75, "icons", "potion-buff-" + t.element));
+					i.anchor.setTo(.5, .5);
+					var a = this.game.add.tween(i).to({
+							alpha: 0
+						}, 1300, Phaser.Easing.Quadratic.Out),
+						s = this.game.add.tween(i.scale).to({
+							x: 4,
+							y: 4
+						}, 1300, Phaser.Easing.Quadratic.Out);
+					a.start(), s.onComplete.add(function () {
+						i.destroy()
+					}, i), s.start()
+				}
+			}
+			var r = "",
+				o = Math.random() < this.CRITICAL_HIT_THRESHOLD + this.criticalThresholdBonus && "PVP" !== this.game.state.current || "epic-attack" === this.atk.type;
+			o ? (this.damageDone = Math.round(1.50 * this.damageDone), this.game.prodigy.effects.characterImage(this.game.prodigy.create.sprite(this.target.x, this.source.y + 50, "battle", "text-critical-hit"), 1e3 + this.shieldTime), r = "critical-hit") : r = "normal-hit", this.processStars(), this.lastTargetHp = this.target.source.getCurrentHearts(), this.target.source.changeCurrentHearts(-this.damageDone), this.game.prodigy.effects.characterText("-" + this.damageDone, this.target.x, this.source.y, this.shieldTime, {
+				size: 1,
+				font: "battle",
+				mono: 44
+			}), this.game.prodigy.audio.playSFX(Prodigy.Controller.AudioController.SFX_PACKS.BATTLE, r);
+			var n = null;
+			o || 0 !== this.target.source.modifiers.ignoreElement || (this.game.prodigy.attacks.isStrong(this.atk.element, this.target.source.getElement()) ? n = "Powerful!" : this.game.prodigy.attacks.isWeak(this.atk.element, this.target.source.getElement()) && (n = "Weak...")), Util.isDefined(n) && (this.delayComplete = !0, this.game.prodigy.effects.characterText(n, this.target.x, this.source.y + 50, 1e3 + this.shieldTime));
 		}
 	}
 }
