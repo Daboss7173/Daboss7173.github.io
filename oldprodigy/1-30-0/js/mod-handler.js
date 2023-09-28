af6ddb86f4f92cd242f85ba88879bd6c7ec8b557
Daboss7173 <75582409+Daboss7173@users.noreply.github.com>
2023-09-28 17:54:02 -0500

    Added in the mod enhancements from 1-50-0

--- oldprodigy/1-30-0/js/mod-handler.js
+++ oldprodigy/1-30-0/js/mod-handler.js
@@ -1,43 +1,66 @@
 var GameMods = {
-	available: [
-		{
-			id: "WalkSpeed",
-			patch: "initWalkSpeedMod"
-		},
-		{
-			id: "FastGameSpeed",
-			patch: "initFastGameSpeedMod"
-		}
-	]
+	available: [{
+		id: "WalkSpeed",
+		patch: "initWalkSpeedMod"
+	}, {
+		id: "FastGameSpeed",
+		patch: "initFastGameSpeedMod"
+	}]
 };
-
 class ModHandler {
 	constructor(e) {
-		this.game = e
-	}
-	
-	log(message) {
-		if (Util.isDefined(message)) {
-			console.log("%c %c %c Old Prodigy Mod Handler | " + message + " %c %c ",
-			"background: #bdb",
-			"background: #4a4",
-			"background: #172; color: #FFF",
-			"background: #4a4",
-			"background: #bdb")
+		this.game = e, this.animations = new class {
+			constructor() {
+				this.animations = []
+			}
+			getAll() {
+				let e = [];
+				for (let t = 0; t < this.animations.length; t++) e.push(this.animations[t].animation);
+				return e
+			}
+			register(e, t) {
+				this.animations.push({
+					animation: e,
+					baseSpeed: t
+				})
+			}
+			clear() {
+				this.animations = []
+			}
+		}, this.tweens = new class {
+			constructor() {
+				this.tweens = []
+			}
+			getAll() {
+				return this.tweens
+			}
+			getTweens() {
+				let e = [];
+				for (let t = 0; t < this.tweens.length; t++) e.push(this.tweens[t].tween);
+				return e
+			}
+			register(e, t, i, a) {
+				this.tweens.push({
+					tween: t,
+					_parent: e,
+					baseDelay: i,
+					baseDuration: a
+				})
+			}
+			clear() {
+				this.tweens = []
+			}
 		}
 	}
-	
-	error(message) {
-		if (Util.isDefined(message)) {
-			console.log("%c %c %c Old Prodigy Mod Handler | " + message + " %c %c ",
-			"background: #dbb",
-			"background: #b66",
-			"background: #a11; color: #FFF",
-			"background: #b66",
-			"background: #dbb")
-		}
+	log(e) {
+		Util.isDefined(e) && console.log("%c %c %c " + e + " %c %c ", "background: #1724", "background: #172a", "background: #172f; color: #FFF", "background: #172a", "background: #1724")
 	}
-	
+	info(e) {
+		Util.isDefined(e) && console.log("%c %c %c " + e + " %c %c ", "background: #14b4", "background: #14ba", "background: #14bf; color: #FFF", "background: #14ba", "background: #14b4")
+	}
+	error(e) {
+		Util.isDefined(e) && console.log("%c %c %c " + e + " %c %c ", "background: #a114", "background: #a11a", "background: #a11f; color: #FFF", "background: #a11a", "background: #a114")
+	}
 	initWalkSpeedMod() {
 		this.game.prodigy.player.walkSpeed = 1,
 		Prodigy.Container.CreatureContainer.prototype.setPath = function (e, t, i) {
@@ -76,46 +99,68 @@ class ModHandler {
 			})
 		}
 	}
-	
 	initFastGameSpeedMod() {
-		var i = Phaser.TweenManager.prototype.add,
-			e = this.game,
-			t = 3;
-		Phaser.TweenManager.prototype.add = function(a) {
-			a.timeScale = t, i.call(this, a)
-		};
-		var a = Phaser.Timer.prototype.add;
-		Phaser.Timer.prototype.add = function(d, b, c) {
-			d /= t, a.call(this, d, b, c)
-		};
-		var s = Phaser.Tween.prototype.delay;
-		Phaser.Tween.prototype.delay = function(a, b) {
-			a /= t; return s.call(this, a, b)
-		}
+		var e = Phaser.TweenManager.prototype.add,
+			t = this.game,
+			i = Phaser.Timer.prototype.add,
+			a = Phaser.Tween.prototype.delay,
+			s = Phaser.Tween.prototype.to,
+			r = (Boot.prototype.update, Phaser.AnimationManager.prototype.play),
+			o = Prodigy.TweenController.prototype.set,
+			n = Phaser.AnimationManager.prototype.add,
+			l = Phaser.Game.prototype.update,
+			d = this.animations,
+			h = this.tweens;
+		Phaser.Game.prototype.update = function(e) {
+			for (let e = 0; e < d.animations.length; e++) !Util.isDefined(d.animations[e].animation._parent) && d.animations.splice(e, 1);
+			for (let e = 0, t = h.getAll(); e < t.length; e++) !t[e]._parent.src.alive && t.splice(e, 1);
+			return l.call(this, e)
+		}, window.setGameSpeed = function(l) {
+			var g = l;
+			if (g < .1) ModHooks.error("Supplied speed multiplier is too low. Try a larger speed value.");
+			else {
+				if (Phaser.TweenManager.prototype.add = function(t) {
+						t.timeScale = g, e.call(this, t)
+					}, Phaser.Timer.prototype.add = function(e, t, a) {
+						return e /= g, i.call(this, e, t, a)
+					}, Phaser.Tween.prototype.delay = function(e, t) {
+						return e /= g, a.call(this, e, t)
+					}, Phaser.Tween.prototype.to = function(e, t, i, a, r, o, n) {
+						return Util.isDefined(r) && (r /= g), s.call(this, e, t, i, a, r, o, n)
+					}, Phaser.AnimationManager.prototype.add = function(e, t, i, a, s) {
+						let r = 10;
+						Util.isDefined(i) && (r = i, i *= g);
+						let o = n.call(this, e, t, i, a, s);
+						return d.register(o, r), o
+					}, Phaser.AnimationManager.prototype.play = function(e, t, i, a) {
+						return Util.isDefined(t) && !isNaN(t) && (t *= g), r.call(this, e, t, i, a)
+					}, Prodigy.TweenController.prototype.set = function(e, t, i, a) {
+						let s = i,
+							r = a;
+						Util.isDefined(i) && (i /= g), Util.isDefined(a) && (a /= g);
+						let n = o.call(this, e, t, i, a);
+						return h.register(this, this.tweens[this.tweens.length - 1], s, r), n
+					}, Util.isDefined(t.tweens))
+					for (var c = t.tweens.getAll(), p = 0; p < c.length; p++) c[p].timeScale = g;
+				if (Util.isDefined(d))
+					for (var u = d.getAll(), m = 0; m < u.length; m++) u[m].speed = g * (Util.isDefined(d.animations[m].baseSpeed) ? d.animations[m].baseSpeed : 10);
+				if (Util.isDefined(h))
+					for (var f = h.getAll(), y = 0, v = h.getTweens(); y < f.length; y++) v[y].delay = f[y].baseDelay / g, v[y].duration = v[y].frames.length * f[y].baseDelay * f[y].baseDuration / g
+			}
+		}, window.setGameSpeed(3), setTimeout((() => {
+			this.info('Use "setGameSpeed(speed)" to change the game speed at anytime.')
+		}), 1e3)
 	}
 }
 
 function checkForMods(e, t) {
-	window.ModHooks = new ModHandler(e);
-	ModHooks.log("Checking for mods...");
-	var c = 0;
-	for(var i = 0; i < GameMods.available.length; i += 1) {
-		if (t.includes(GameMods.available[i].id)) {
-			try {
-				var p = GameMods.available[i].patch,
-					f = ModHooks[p];
-				f.call(ModHooks);
-				ModHooks.log("Mod \"" + GameMods.available[i].id + "\" successfully applied!");
-				c += 1
-			} catch (e) {
-				ModHooks.error("Error occured while applying mod \"" + GameMods.available[i].id + "\" to Prodigy!");
-				console.error(e)
-			}
+	window.ModHooks = new ModHandler(e), ModHooks.log("Checking for mods...");
+	for (var i = 0, a = 0; a < GameMods.available.length; a += 1)
+		if (t.includes(GameMods.available[a].id)) try {
+			var s = GameMods.available[a].patch;
+			ModHooks[s].call(ModHooks), ModHooks.log('Mod "' + GameMods.available[a].id + '" successfully applied!'), i += 1
+		} catch (e) {
+			ModHooks.error('Error occured while applying mod "' + GameMods.available[a].id + '" to Prodigy!'), console.error(e)
 		}
-	};
-	if (c > 0) {
-		ModHooks.log("Applied (" + c + ") mods to the game")
-	} else {
-		ModHooks.log("No mods have been applied")
-	}
+	i > 0 ? ModHooks.log("Applied (" + i + ") mods to the game") : ModHooks.log("No mods have been applied")
 }
